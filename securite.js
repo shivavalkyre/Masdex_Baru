8cfa281841b1615f0f57135f42450f04a29c4bdc
Arif Fadhila SN <afadhilasn15@gmail.com>
2022-02-09 17:38:12 +0700

    securite

--- securite.js
+++ securite.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 
 const createSecurite = (request, response) => {
-  const { no_jurnal, tgl_jurnal, jenis_securite, waktu_kejadian, sumber_info, ket_lainnya, mmsi, status_nav, info_tambahan, degree1, minute1, second1, direction1, degree2, minute2, second2, direction2, lokasi_terlapor, degree3, minute3, second3, direction3, degree4, minute4, second4, direction4, pelabuhan_asal, pelabuhan_tujuan, desc_securite, memerlukan_tindakan, mob, nohp1, secc_officer, nohp2, deskripsi_tindakan,voyage_id } 
+  const { jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 } 
   = request.body
-  pool.query('INSERT INTO tbl_insaf_securite (no_jurnal, tanggal, jenis_securite, waktu_kejadian, sumber_informasi_awal, keterangan_lainnya, mmsi, status_navigasi, info_tambahan, degree1, minute1, second1, direction1, degree2, minute2, second2, direction2, lokasi_terlapor, degree3, minute3, second3, direction3, degree4, minute4, second4, direction4, pelabuhan_asal, pelabuhan_tujuan, descripsi_securite, memerlukan_tindakan, master_onboard, nohp1, seccond_officer, nohp2, deskripsi_tindakan,voyage_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,$10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36)'
-  , [no_jurnal, "'"+tgl_jurnal+"'", jenis_securite, "'"+waktu_kejadian+"'", sumber_info, ket_lainnya, mmsi, status_nav, info_tambahan, degree1, minute1, second1, direction1, degree2, minute2, second2, direction2, lokasi_terlapor, degree3, minute3, second3, direction3, degree4, minute4, second4, direction4, pelabuhan_asal, pelabuhan_tujuan,desc_securite, memerlukan_tindakan, mob, nohp1, secc_officer, nohp2, deskripsi_tindakan,voyage_id], (error, results) =>{
+  pool.query('INSERT INTO tbl_insaf_securite (jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,seccond_officer ,nohp2) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,$10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)'
+  , [jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2], (error, results) =>{
     if (error) {
       throw error
       //response.status(201).send(error)
@@ -20,25 +20,23 @@ const createSecurite = (request, response) => {
     }
     else
     {
-      pool.query('SELECT id FROM tbl_insaf_securite ORDER BY id DESC limit 1', (error1, results1) => {
-        if (error1) {
-          throw error1
-        }
-        else{
-          var myid = results1.rows[0].id;
-        }
-
-        response.status(200).send({status:true,data:`Added Securite Successfuly !`, last_id: myid})
+      pool.query('SELECT id FROM tbl_insaf_securite ORDER BY id DESC LIMIT 1',  (error, results) => {
+          if (error) 
+          {
+              throw error
+          }
+          response.status(200).send({success:true,data: results.rows[0].id})
       })
+
     }
   })
 }
 
 const createSecuriteDetail = (request, response) => {
-  const { securite_id, nama_pelapor, tgl_lapor, info_tambahan, instansi, kontak, created_by } 
+  const { securite_id, nama_pelapor, tgl_lapor, info_tambahan, created_by } 
   = request.body
-  pool.query('INSERT INTO tbl_insaf_securite_detail (securite_id, nama_pelapor, tgl_lapor, info_tambahan, instansi, kontak, created_by) VALUES ($1, $2, $3, $4, $5, $6, $7)'
-  , [securite_id, nama_pelapor, tgl_lapor, info_tambahan, instansi, kontak, created_by], (error, results) =>{
+  pool.query('INSERT INTO tbl_insaf_securite_detail (securite_id, nama_pelapor, tgl_lapor, info_tambahan, created_by) VALUES ($1, $2, $3, $4, $5)'
+  , [securite_id, nama_pelapor, tgl_lapor, info_tambahan, created_by], (error, results) =>{
     if (error) {
       // throw error
       response.status(201).send(error)
@@ -55,8 +53,6 @@ const createSecuriteDetail = (request, response) => {
     }      
   })
 }
-
-
   
 const getSecurite = (request, response) => {
   const {page,rows} = request.body
@@ -75,7 +71,7 @@ const getSecurite = (request, response) => {
 
   //  var sql= 'SELECT s.*, d.id id_detail, d.securite_id securite_id, d.nama_pelapor nama_pelapor, d.tgl_lapor tgl_lapor, d.info_tambahan info_tambahan FROM tbl_insaf_securite s INNER JOIN tbl_insaf_securite_detail d ON s.id = d.securite_id ORDER BY id ASC LIMIT '  + rows_req + ' OFFSET ' + offset
   //  var sql= 'SELECT * from insaf_securitexsecuritedetail ORDER BY id DESC LIMIT '  + rows_req + ' OFFSET ' + offset
-    var sql= 'SELECT * from tbl_insaf_securite WHERE is_delete = false'
+    var sql= 'SELECT s.*, js.jenis_securite, ji.jenis_informasi_securite from tbl_insaf_securite s left join tbl_insaf_jenis_securite js on js.id = s.jenis_securite left join tbl_insaf_jenis_informasi_securite ji on ji.id = s.sumber_informasi_awal WHERE s.is_delete = false'
     // var sql= 'SELECT * from insaf_securitexsecuritedetails'
     pool.query(sql ,(error, results) => {
       if (error) {
@@ -92,12 +88,11 @@ const getSecurite = (request, response) => {
 
 }
 
-
 const getSecuriteById = (request, response) => {
   const id = parseInt(request.params.id)
   // response.status(200).json(id)
   // pool.query('SELECT s.*, d.id id_detail, d.securite_id securite_id, d.nama_pelapor nama_pelapor, d.tgl_lapor tgl_lapor, d.info_tambahan info_tambahan FROM tbl_insaf_securite s INNER JOIN tbl_insaf_securite_detail d ON s.id = d.securite_id WHERE s.id = $1', [id], (error, results) =>  { 
-  pool.query('SELECT * FROM tbl_insaf_securite WHERE id = $1', [id], (error, results) => {
+  pool.query('SELECT s.*, m.ship_name, m.imo, m.call_sign, m.flag, ji.jenis_informasi_securite, js.jenis_securite FROM tbl_insaf_securite s join tbl_masdex_kapal m on s.mmsi = m.mmsi join tbl_insaf_jenis_informasi_securite ji on ji.id = s.sumber_informasi_awal join tbl_insaf_jenis_securite js on js.id = s.jenis_securite WHERE s.id = $1', [id], (error, results) => {
     if (error) {
       throw error
     }
@@ -107,7 +102,7 @@ const getSecuriteById = (request, response) => {
 
 const getSecuriteDetailById = (request, response) => {
   const id = parseInt(request.params.id)
-  pool.query('SELECT id,nama_pelapor,kontak_pelapor,instansi_pelapor,tanggal_lapor,info_tambahan_pelapor FROM insaf_securite_detail WHERE securite_id = $1', [id], (error, results) => {
+  pool.query('SELECT s.* FROM tbl_insaf_securite_detail s WHERE s.securite_id = $1', [id], (error, results) => {
     if (error) {
       throw error
     }
@@ -170,22 +165,46 @@ const getSecuriteByRange = (request, response) => {
 
 
 const updateSecurite = (request,response) => {
-const id = parseInt(request.params.id)
-const { no_jurnal, tgl_jurnal, jenis_securite, waktu_kejadian, sumber_informasi_awal, keterangan_lainnya, mmsi, status_bernavigasi, info_tambahan, degree1, minute1, second1, direction1, degree2, minute2, second2, direction2, lokasi_terlapor, degree3, minute3, second3, direction3, degree4, minute4, second4, direction4, pelabuhan_asal, pelabuhan_tujuan, deskripsi_laporan_securite, memerlukan_tindakan, deskripsi_tindakan,mob, nohp1, second_officer, nohp2,voyage_id } = request.body
-var updated_at = new Date()
-//console.log(sumber_informasi_awal);
-//console.log(no_jurnal+","+ tgl_jurnal+","+ jenis_securite+","+ waktu_kejadian+"," + sumber_informasi_awal +"," +keterangan_lainnya+"," +mmsi+","+ status_bernavigasi +","+ info_tambahan+","+ degree1+","+ minute1+","+ second1+","+ direction1+","+ degree2+","+ minute2+","+ second2+","+ direction2+","+ lokasi_terlapor+","+ degree3+","+ minute3+","+ second3+","+ direction3+","+ degree4+","+ minute4+","+ second4+","+ direction4+","+ pelabuhan_asal+","+ pelabuhan_tujuan+","+ deskripsi_laporan_securite+","+memerlukan_tindakan+","+ mob+","+ nohp1+","+ second_officer+","+ nohp2,deskripsi_tindakan);
-pool.query(
-  'UPDATE tbl_insaf_securite SET no_jurnal = $1, tanggal = $2, jenis_securite = $3, waktu_kejadian = $4, sumber_informasi_awal = $5, keterangan_lainnya = $6, mmsi = $7, status_navigasi = $8, informasi_tambahan = $9, degree1 = $10, minute1 = $11, second1 = $12, direction1 = $13, degree2 = $14, minute2 = $15, second2 = $16, direction2 = $17, lokasi_terlapor = $18, degree3 = $19, minute3 = $20, second3 = $21, direction3 = $22, degree4 = $23, minute4 = $24, second4 = $25, direction4 = $26, pelabuhan_asal = $27, pelabuhan_tujuan = $28, descripsi_securite = $29, memerlukan_tindakan = $30, master_onboard = $31, nohp1 = $32, seccond_officer = $33, nohp2 = $34, updated_at = $35,deskripsi_tindakan=$36,voyage_id=$37 WHERE id = $38',
-  [no_jurnal, tgl_jurnal, jenis_securite, waktu_kejadian, sumber_informasi_awal, keterangan_lainnya, mmsi, status_bernavigasi, info_tambahan, degree1, minute1, second1, direction1, degree2, minute2, second2, direction2, lokasi_terlapor, degree3, minute3, second3, direction3, degree4, minute4, second4, direction4, pelabuhan_asal, pelabuhan_tujuan, deskripsi_laporan_securite, memerlukan_tindakan, mob, nohp1, second_officer, nohp2, updated_at,deskripsi_tindakan,voyage_id,id],(error, results) => {
-    if (error) {
-      throw error
-    }
+  const id = parseInt(request.params.id)
+  const { jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 } = request.body
+  var updated_at = new Date()
 
-    response.status(200).send({status:true,data:`User modified with ID: ${id}`})
+  pool.query(
+    'UPDATE tbl_insaf_securite SET jenis_securite = $1 ,waktu_kejadian = $2 ,sumber_informasi_awal = $3 ,info_tambahan = $4 ,mmsi = $5 ,status_navigasi = $6 ,lokasi_terlapor = $7 ,degree1 = $8 ,minute1 = $9 ,second1 = $10 ,direction1 = $11 ,degree2 = $12 ,minute2 = $13 ,second2 = $14 ,direction2 = $15 ,descripsi_securite = $16 ,keterangan_pertolongan = $17 ,master_onboard = $18 ,nohp1 = $19 ,seccond_officer = $20 ,nohp2 = $21 WHERE id = $22',
+    [jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2,id],(error, results) => {
+      if (error) {
+        throw error
 
-  })
-  //response.status(200).send({status:true,data:`User modified with ID: ${id}`})
+      }else{
+
+        pool.query('DELETE FROM tbl_insaf_securite_detail where securite_id=$1'
+        , [id], (error, results) => {
+            if (error) {
+
+                if (error.code == '23505') {
+                    //console.log("\n ERROR! \n Individual with name: " + body.fname + " " + body.lname + " and phone #: " + body.phone + " is a duplicate member. \n");
+                    response.status(400).send('Duplicate data')
+                    return;
+                }
+            } else {
+                
+                pool.query('SELECT id FROM tbl_insaf_securite where id=$1 LIMIT 1', [id],  (error, results) => {
+                    if (error) 
+                    {
+                        throw error
+                    }
+                    response.status(200).send({success:true,data: results.rows[0].id})
+                })
+                // response.status(200).send({ success: true, data: 'data berhasil diperbarui' })
+            }
+
+        })
+      }
+
+      // response.status(200).send({status:true,data:`User modified with ID: ${id}`})
+
+    })
+    //response.status(200).send({status:true,data:`User modified with ID: ${id}`})
 }
 
 
