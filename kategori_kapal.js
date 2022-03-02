7263ee346061cbf9d5b42f53a9f34e15dc29cabd
shivavalkyre <syerif@gmail.com>
2022-03-02 11:51:44 +0700

    syerif20220302-01

---
+++ kategori_kapal.js
@@ -0,0 +1,40 @@
+const pool = require('./dbCon');
+const fs = require('fs');
+const path = require('path')
+const base_url = process.env.base_url
+
+const read = (request, response) => {
+
+    const { page, rows } = request.body
+    var page_req = page || 1
+    var rows_req = rows || 10
+    var offset = (page_req - 1) * rows_req
+    var res = []
+    var items = []
+
+
+    pool.query('SELECT count(*) as total FROM tbl_masdex_kategori_kapal where is_delete=false', (error, results) => {
+        if (error) {
+            throw error
+        }
+        //console.log(results.rows[0].total)
+        res.push({ total: results.rows[0].total })
+
+        var sql = 'SELECT * FROM tbl_masdex_kategori_kapal where is_delete=false ORDER BY id ASC'
+        pool.query(sql, (error, results) => {
+            if (error) {
+                throw error
+            }
+            items.push({ rows: results.rows })
+            res.push(items)
+            response.status(200).send({ success: true, data: res })
+        })
+
+    })
+
+}
+
+module.exports = {
+read
+
+}
