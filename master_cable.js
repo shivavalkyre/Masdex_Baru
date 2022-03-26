cfe889a95bef9413393daf90cea1d7129b767e9b
shivavalkyre <syerif@gmail.com>
2022-03-26 16:18:16 +0700

    syerif20220326-01

--- master_cable.js
+++ master_cable.js
@@ -396,8 +396,7 @@ const kurs_tengah = (request, response) =>
 {
                     // get kurs tengah
                     var myInt = setInterval(function () {
-                        let url ='http://api-insafmasdex.disnavpriok.id:3012/api/V1/kurs'
-                        // let url ='http://localhost:3012/api/V1/kurs'
+                        let url ='http://localhost:3012/api/V1/kurs'
                         req(url, function (err, res, body) {
                             //console.log(body)
                             var mykurs = JSON.parse(body)
@@ -451,8 +450,7 @@ const cek_total_tagihan= (request, response)  =>
         {
             // get kurs tengah
                  var myInt = setInterval(function () {
-                    let url ='http://api-insafmasdex.disnavpriok.id:3012/api/V1/kurs'
-                    // let url ='http://localhost:3012/api/V1/kurs'
+                    let url ='http://localhost:3012/api/V1/kurs'
                     req(url, function (err, res, body) {
                         var mykurs = JSON.parse(body)
                         //console.log(mykurs.data)
@@ -865,8 +863,7 @@ const cek_total_tagihan= (request, response)  =>
 
         // get kurs tengah
         var myInt = setInterval(function () {
-            let url ='http://api-insafmasdex.disnavpriok.id:3012/api/V1/kurs'
-            // let url ='http://localhost:3012/api/V1/kurs'
+          let url ='http://localhost:3012/api/V1/kurs'
           req(url, function (err, res, body) {
               var mykurs = JSON.parse(body)
               //console.log(mykurs.data)
