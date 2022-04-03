076eaaf5c2f3ca5ea2177739fccf7a7f2d8806be
Arif Fadhila SN <afadhilasn15@gmail.com>
2022-04-03 14:44:08 +0700

    securite update

--- securite.js
+++ securite.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 
 const createSecurite = (request, response) => {
-  const { jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 , title, pelabuhan_asal, pelabuhan_tujuan} 
+  const { jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 , title, pelabuhan_asal, pelabuhan_tujuan, voyage_id} 
   = request.body
-  pool.query('INSERT INTO tbl_insaf_securite (jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,seccond_officer ,nohp2 , title, pelabuhan_asal, pelabuhan_tujuan) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,$10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24)'
-  , [jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 ,title ,pelabuhan_asal ,pelabuhan_tujuan], (error, results) =>{
+  pool.query('INSERT INTO tbl_insaf_securite (jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,seccond_officer ,nohp2 , title, pelabuhan_asal, pelabuhan_tujuan, voyage_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,$10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)'
+  , [jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 ,title ,pelabuhan_asal ,pelabuhan_tujuan, voyage_id], (error, results) =>{
     if (error) {
       throw error
       //response.status(201).send(error)
@@ -104,7 +104,7 @@ const getSecuriteByVoyage = (request, response) => {
   const id = parseInt(request.params.id)
   // response.status(200).json(id)
   // pool.query('SELECT s.*, d.id id_detail, d.securite_id securite_id, d.nama_pelapor nama_pelapor, d.tgl_lapor tgl_lapor, d.info_tambahan info_tambahan FROM tbl_insaf_securite s INNER JOIN tbl_insaf_securite_detail d ON s.id = d.securite_id WHERE s.id = $1', [id], (error, results) =>  { 
-  pool.query('SELECT s.*, na.ais_status_navigation, pa.nama_pelabuhan as last_port, pl.nama_pelabuhan as next_port, m.ship_name, m.imo, m.call_sign, m.flag, ji.jenis_informasi_securite, js.jenis_securite FROM tbl_insaf_securite s left join tbl_masdex_pelabuhan pa on pa.id = s.pelabuhan_asal left join tbl_masdex_pelabuhan pl on pl.id = s.pelabuhan_tujuan  left join tbl_masdex_kapal m on s.mmsi = m.mmsi left join tbl_insaf_jenis_informasi_securite ji on ji.id = s.sumber_informasi_awal left join tbl_insaf_jenis_securite js on js.id = s.jenis_securite left join tbl_insaf_ais_status_navigation na on na.id = s.status_navigasi WHERE s.voyage_id = $1', [id], (error, results) => {
+  pool.query('SELECT s.*, na.ais_status_navigation, pa.nama_pelabuhan as last_port, pl.nama_pelabuhan as next_port, m.ship_name, m.imo, m.call_sign, m.flag, ji.jenis_informasi_securite, js.jenis_securite FROM tbl_insaf_securite s left join tbl_masdex_pelabuhan pa on pa.id = s.pelabuhan_asal left join tbl_masdex_pelabuhan pl on pl.id = s.pelabuhan_tujuan  left join tbl_masdex_kapal m on s.mmsi = m.mmsi left join tbl_insaf_jenis_informasi_securite ji on ji.id = s.sumber_informasi_awal left join tbl_insaf_jenis_securite js on js.id = s.jenis_securite left join tbl_insaf_ais_status_navigation na on na.id = s.status_navigasi WHERE s.voyage_id = $1 and s.is_delete=false ', [id], (error, results) => {
     if (error) {
       throw error
     }
@@ -178,12 +178,12 @@ const getSecuriteByRange = (request, response) => {
 
 const updateSecurite = (request,response) => {
   const id = parseInt(request.params.id)
-  const { jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 , title, pelabuhan_asal, pelabuhan_tujuan } = request.body
+  const { jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer ,nohp2 , title, pelabuhan_asal, pelabuhan_tujuan, voyage_id } = request.body
   var updated_at = new Date()
 
   pool.query(
-    'UPDATE tbl_insaf_securite SET jenis_securite = $1 ,waktu_kejadian = $2 ,sumber_informasi_awal = $3 ,info_tambahan = $4 ,mmsi = $5 ,status_navigasi = $6 ,lokasi_terlapor = $7 ,degree1 = $8 ,minute1 = $9 ,second1 = $10 ,direction1 = $11 ,degree2 = $12 ,minute2 = $13 ,second2 = $14 ,direction2 = $15 ,descripsi_securite = $16 ,keterangan_pertolongan = $17 ,master_onboard = $18 ,nohp1 = $19 ,seccond_officer = $20 ,nohp2 = $21 , title = $23, pelabuhan_asal = $24, pelabuhan_tujuan = $25 WHERE id = $22',
-    [jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer , nohp2, id, title, pelabuhan_asal, pelabuhan_tujuan],(error, results) => {
+    'UPDATE tbl_insaf_securite SET jenis_securite = $1 ,waktu_kejadian = $2 ,sumber_informasi_awal = $3 ,info_tambahan = $4 ,mmsi = $5 ,status_navigasi = $6 ,lokasi_terlapor = $7 ,degree1 = $8 ,minute1 = $9 ,second1 = $10 ,direction1 = $11 ,degree2 = $12 ,minute2 = $13 ,second2 = $14 ,direction2 = $15 ,descripsi_securite = $16 ,keterangan_pertolongan = $17 ,master_onboard = $18 ,nohp1 = $19 ,seccond_officer = $20 ,nohp2 = $21 , title = $23, pelabuhan_asal = $24, pelabuhan_tujuan = $25, voyage_id = $26 WHERE id = $22',
+    [jenis_securite ,waktu_kejadian ,sumber_informasi_awal ,info_tambahan ,mmsi ,status_navigasi ,lokasi_terlapor ,degree1 ,minute1 ,second1 ,direction1 ,degree2 ,minute2 ,second2 ,direction2 ,descripsi_securite ,keterangan_pertolongan ,master_onboard ,nohp1 ,second_officer , nohp2, id, title, pelabuhan_asal, pelabuhan_tujuan, voyage_id],(error, results) => {
       if (error) {
         throw error
 
